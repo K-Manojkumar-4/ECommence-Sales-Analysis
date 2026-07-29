32654ab5a2828fef2516fea158f230ac26505804
Manoj Kumar K <143381013+K-Manojkumar-4@users.noreply.github.com>
2026-07-29 22:37:38 +0530

    Update business_analysis.sql

--- business_analysis.sql
+++ business_analysis.sql
@@ -9,146 +9,119 @@ USE amazon_dataset;
 -- 1. Basic Retrieval
 -- Find all Completed orders placed in March 2025
 -- ------------------------------------------------------------
-SELECT 
-    OrderID,
-    Date,
-    Product,
-    TotalSales
-FROM amazon_sales
-WHERE Date LIKE '2025-03%'
-  AND Status = 'Completed'
-ORDER BY Date DESC;
 
+select OrderID , Date , Product , TotalSales from amazon_sales
+where Date like '2025-03-%'
+order by Date desc;
 
 -- ------------------------------------------------------------
 -- 2. Aggregation
 -- Total revenue, quantity sold, and average order value
 -- ------------------------------------------------------------
-SELECT 
-    SUM(TotalSales)                    AS total_revenue,
-    SUM(Quantity)                      AS total_quantity_sold,
-    ROUND(AVG(TotalSales), 2)          AS average_order_value
-FROM amazon_sales;
 
+select sum(TotalSales) as total_revenue ,
+ sum(Quantity) as total_quantity_sold ,
+ round(avg(TotalSales),2) as average_order_value 
+ from amazon_sales;
 
 -- ------------------------------------------------------------
 -- 3. Category Analysis
 -- ------------------------------------------------------------
-SELECT 
-    Category,
-    SUM(TotalSales)                    AS total_sales,
-    COUNT(*)                           AS number_of_orders
-FROM amazon_sales
-GROUP BY Category
-ORDER BY total_sales DESC;
 
+select Category,
+ sum(TotalSales) as total_sales ,
+ count(*) as number_of_orders
+ from amazon_sales
+ group by Category
+ order by total_sales desc;
 
 -- ------------------------------------------------------------
 -- 4. Top 5 Products by Quantity Sold
 -- ------------------------------------------------------------
-SELECT 
-    Product,
-    SUM(Quantity)                      AS total_quantity_sold
-FROM amazon_sales
-GROUP BY Product
-ORDER BY total_quantity_sold DESC
-LIMIT 5;
-
+select Product , sum(Quantity) as total_quantity_sold from amazon_sales
+group by Product
+order by total_quantity_sold desc
+limit 5;
 
 -- ------------------------------------------------------------
 -- 5. Top 5 Customers by Revenue
 -- ------------------------------------------------------------
-SELECT 
-    CustomerName,
-    SUM(TotalSales)                    AS total_sales,
-    COUNT(*)                           AS number_of_orders
-FROM amazon_sales
-GROUP BY CustomerName
-ORDER BY total_sales DESC
-LIMIT 5;
 
+select CustomerName , sum(TotalSales) as total_sales , count(*) as number_of_orders from amazon_sales
+group by CustomerName
+order by total_sales desc
+limit 5;
 
 -- ------------------------------------------------------------
 -- 6. Payment Method Analysis + Revenue Percentage
 -- ------------------------------------------------------------
-SELECT 
-    PaymentMethod,
-    SUM(TotalSales)                    AS total_sales,
-    COUNT(*)                           AS number_of_orders,
-    ROUND(SUM(TotalSales) * 100.0 / (SELECT SUM(TotalSales) FROM amazon_sales), 2) 
-                                       AS revenue_percentage
-FROM amazon_sales
-GROUP BY PaymentMethod
-ORDER BY total_sales DESC;
 
+select PaymentMethod ,
+ sum(TotalSales) as total_sales ,
+ count(*) as number_of_orders ,
+ round(sum(TotalSales)*100.0/(select sum(TotalSales) from amazon_sales),2) as revenue_percentage
+ from amazon_sales
+ group by PaymentMethod
+ order by total_sales desc;
 
 -- ------------------------------------------------------------
 -- 7. Status-wise Performance + Completion Rate
 -- ------------------------------------------------------------
-SELECT 
-    Status,
-    COUNT(*)                           AS number_of_orders,
-    SUM(TotalSales)                    AS total_sales,
-    ROUND(COUNT(CASE WHEN Status = 'Completed' THEN 1 END) * 100.0 / COUNT(*), 2) 
-                                       AS completion_rate
-FROM amazon_sales
-GROUP BY Status;
 
+select Status ,
+count(*) as number_of_orders ,
+sum(TotalSales) as total_sales ,
+round(count(case when Status = 'Completed' then 1 end ) * 100.0 / count(*) ,2) as completion_rate 
+from amazon_sales
+group by Status;
 
 -- ------------------------------------------------------------
 -- 8. Top 5 Cities by Sales
 -- ------------------------------------------------------------
-SELECT 
-    CustomerLocation,
-    SUM(TotalSales)                    AS total_sales,
-    ROUND(AVG(TotalSales), 2)          AS average_order_value,
-    COUNT(*)                           AS number_of_orders
-FROM amazon_sales
-GROUP BY CustomerLocation
-ORDER BY total_sales DESC
-LIMIT 5;
 
+select CustomerLocation ,
+ sum(TotalSales) as total_sales ,
+ round(avg(TotalSales),2) as average_order_value ,
+ count(*) as number_of_orders 
+ from amazon_sales
+group by CustomerLocation
+order by total_sales desc 
+limit 5;
 
 -- ------------------------------------------------------------
 -- 9. Monthly Sales Trend (Feb – Apr 2025)
 -- ------------------------------------------------------------
-SELECT 
-    DATE_FORMAT(Date, '%Y-%m')         AS month,
-    SUM(TotalSales)                    AS total_sales,
-    COUNT(*)                           AS number_of_orders
-FROM amazon_sales
-WHERE Date BETWEEN '2025-02-01' AND '2025-04-30'
-GROUP BY month
-ORDER BY month;
 
+select date_format(Date , '%m') as month ,
+sum(TotalSales) as total_sales ,
+count(*) as number_of_orders 
+ from amazon_sales
+where Date between '2025-02-01' and '2025-04-30'
+group by month
+order by month;
 
 -- ------------------------------------------------------------
 -- 10. Advanced: Top 3 Products per Category (Window Function)
 -- ------------------------------------------------------------
-WITH product_sales AS (
-    SELECT 
-        Category,
-        Product,
-        SUM(TotalSales)                AS total_sales
-    FROM amazon_sales
-    GROUP BY Category, Product
+
+with product_sales as(
+  select Category ,
+         Product ,
+         sum(TotalSales) as total_sales 
+         from amazon_sales
+         group by Category , Product
 ),
-ranked_sales AS (
-    SELECT 
-        Category,
-        Product,
-        total_sales,
-        DENSE_RANK() OVER (
-            PARTITION BY Category 
-            ORDER BY total_sales DESC
-        ) AS rank_value
-    FROM product_sales
+ranked_sales as (
+ select Category , 
+		Product , 
+        total_sales , 
+        dense_rank()over(partition by Category order by total_sales desc) as Rank_value
+		from product_sales 
 )
-SELECT 
-    Category,
-    Product,
-    total_sales,
-    rank_value
-FROM ranked_sales
-WHERE rank_value <= 3
-ORDER BY Category, rank_value;
+select Category ,
+		Product ,
+        total_sales , 
+        Rank_value 
+        from ranked_sales
+where Rank_value <= 3
+order by Category ,Rank_value;;
